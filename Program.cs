23f704fc66f966c691be1c95b72b0dda7114ee0a
fran <francisconoppenau@gmail.cm>
2025-11-15 09:37:06 +0100

    uge10opg6

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Enter words of a sentence one by one including punctuation:");
+
+// liste til at gemme de indtastede ord
+var words = new List<string>();
+
+// uendelig loop, der fortsætter indtil break-kommando udføres
+while (true)
+{
+    var input = Console.ReadLine();
+    
+    // Tilføjer det indtastede input til listen
+    words.Add(input);
+    
+    // Samler inputs i listen til én streng med mellemrum hver input
+    string phrase = string.Join(" ", words);
+
+    if (input.EndsWith("."))
+    {
+       Console.WriteLine($"<{phrase}>");
+       break;
+    }
+}
